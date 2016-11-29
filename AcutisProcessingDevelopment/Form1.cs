e6e4c8993fe7f1645f2b895d533780d96e81c492
Richard Wilson <wilsonras@outlook.com>
2016-11-29 09:07:47 +0000

    1st Working commit

--- AcutisProcessingDevelopment/Form1.cs
+++ AcutisProcessingDevelopment/Form1.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using RASW;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +14,118 @@ namespace AcutisProcessingDevelopment
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string processedTargetFolder = @"C:\ACData\AC_Synchronised_Export_Data";
+        string originalFilesFolder = @"C:\ACData";           // appSet.getValue("DataFolderPath");
+        string configurationFolder = @"C:\ACData\Configuration";
+
+        Dictionary<string, DateTime> lastWriteTimes = new Dictionary<string, DateTime>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckFolderStructure();
+        }
+
+        async void ProcessFiles()
+        {
+            try
+            {
+                AppSettings appSet = new AppSettings(Path.Combine(configurationFolder, "Acutis.xml"));
+                string bk = appSet.getValue("BK");
+                string ak = appSet.getValue("AK");
+                bool uploadToIS = false;
+
+
+                if (bk.Length != 0)
+                    uploadToIS = true;
+
+                Dictionary<string, string> dataParams = new Dictionary<string, string>();
+
+                try
+                {
+                    if (Directory.Exists(originalFilesFolder))
+                    {
+                        CheckFolderStructure();
+                        string dateNow = DateTime.Now.ToString();                               // get the date time now so that each file has the same date time
+                        string[] files = Directory.GetFiles(originalFilesFolder, "*.csv");      // get the list of "csv" file only that are in the folder
+
+                        if (UpdateManager.CheckForNewFileWrites(lastWriteTimes, files) > 0)     // check for any update for AC, if so process files
+                        {
+                            foreach (var file in files)                                         // full paths :)
+                            {
+                                string lastLine = File.ReadLines(file).Last();                  // gets the last line from file.
+                                string[] data = lastLine.Split(';');                            // data[0] = "2015-11-05 15:20:17.072"   data[1] = "20.9364"
+
+                                using (StreamWriter sw = File.AppendText(Path.Combine(processedTargetFolder, Path.GetFileName(file))))
+                                {
+                                    string formattedValue = String.Format("{0:0.00}", Convert.ToDouble(data[1]));       // format to 2 decimal places (does round up 20.94)
+                                    sw.WriteLine(dateNow + "|" + formattedValue);
+
+                                    if (uploadToIS)
+                                        dataParams.Add(Path.GetFileNameWithoutExtension(file), formattedValue);         // if upload to ISD is enabled then async upload the data
+                                }
+                            }
+
+                            if (uploadToIS)
+                            {
+                                await SendMessageToIS(dataParams,ak,bk);
+                            }
+                        }
+                        else
+                        {
+                            dataParams.Clear();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                   // log error to event log
+                }
+
+                //dataParams.Clear(); // clear the dictionary
+            }
+            catch (Exception)
+            {
+                // log the error in service log
+            }
+
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Await.Warning", "CS4014:Await.Warning")]
+        private async Task SendMessageToIS(Dictionary<string, string> dataParams,string AK, string BK)
+        {
+            using (var client = new HttpClient())
+            {
+                var content = new FormUrlEncodedContent(dataParams);
+                var response = await client.PostAsync("https://groker.initialstate.com/api/events?accessKey=V4iVTL0C9Mh5m5gMPP7XLwwF4y3mouYI&bucketKey=JWRCPRMBGJRW", content);  // DEV Bucket
+                //var response = await client.PostAsync("https://groker.initialstate.com/api/events?accessKey=" + AK + "&bucketKey=" + BK, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+            }
+        }
+
+        private void CheckFolderStructure()
+        {
+            if(!Directory.Exists(processedTargetFolder))
+            {
+                Directory.CreateDirectory(processedTargetFolder);
+            }
+
+            if (!Directory.Exists(configurationFolder))     // create hidden folder if not already created
+            {
+                DirectoryInfo di = Directory.CreateDirectory(configurationFolder);
+                di.Attributes = FileAttributes.Directory | FileAttributes.Hidden;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessFiles();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+           ProcessFiles();
         }
     }
 }
---
+++ AcutisProcessingDevelopment/UpdateManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AcutisProcessingDevelopment
+{
+    static class UpdateManager
+    {
+        //Dictionary<string, DateTime> lastWriteTimes = new Dictionary<string, DateTime>();
+     
+        public static int CheckForNewFileWrites(Dictionary<string,DateTime> LastWriteTimes, string[] AKVAconnectExportFilesFolderPath)
+        {
+            int retVal = 0;
+            // creates a backup of the last dictionary and recreates the a new one with the current file write time in it.
+            // returns the count of changes if any of the files have been written to. Returns -1 if errors
+            try
+            {
+                Dictionary<string, DateTime> previousWriteTimes = new Dictionary<string, DateTime>(LastWriteTimes);     // make a copy of the existing "lastWriteTimes" dictionary.
+
+                LastWriteTimes.Clear();                                     // clear the contents of the dictionary
+                foreach (string file in AKVAconnectExportFilesFolderPath)
+                {
+                    DateTime dt = File.GetLastWriteTime(file);              // get the last write time from the file
+                    LastWriteTimes.Add(file, dt);                           // update the dictionary with all files
+                }
+
+                    try
+                    {
+                        foreach (var key in previousWriteTimes.Keys)
+                        {
+                            if (!previousWriteTimes[key].Equals(LastWriteTimes[key]))
+                                retVal += 1;        // +1 to change count
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        return -1;
+                    }
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+            return retVal;
+        }
+    }
+}
