aee066c3155a168c0d7dd934c274362fb7aa3dfd
Richard Wilson <wilsonras@outlook.com>
2016-11-30 12:19:05 +0000

    Last release with file write time trigger.

--- AcutisProcessingDevelopment/Form1.Designer.cs
+++ AcutisProcessingDevelopment/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 10000;
+            this.timer1.Interval = 300000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
             // 
             // button1
